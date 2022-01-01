627638cf7caedb79a5e92f1297312bae248b94b9
Kevin Li <kevinli020508@gmail.com>
2022-01-01 17:21:32 -0500

    Upgrade combine to 4.6.2

--- json/combine/src/main.rs
+++ json/combine/src/main.rs
@@ -13,146 +13,177 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 use bencher::{black_box, Bencher};
 use fnv::FnvHashMap as HashMap;
-use std::hash::Hash;
 use std::str;
 
-use combine::error::ParseError;
-use combine::{Parser, RangeStream, StreamOnce};
-
-use combine::parser::byte::{byte, spaces};
-use combine::parser::choice::{choice, optional};
-use combine::parser::combinator::no_partial;
-use combine::parser::item::{one_of, satisfy_map};
-use combine::parser::range;
-use combine::parser::repeat::{escaped, sep_by};
-use combine::parser::sequence::between;
+use combine::{
+    error::{Commit, ParseError},
+    parser::{
+        char::{char, digit, spaces, string},
+        choice::{choice, optional},
+        function::parser,
+        repeat::{many, many1, sep_by},
+        sequence::between,
+        token::{any, satisfy, satisfy_map},
+    },
+    stream::{
+        buffered,
+        position::{self, SourcePosition},
+        IteratorStream,
+    },
+    EasyParser, Parser, Stream, StreamOnce,
+};
 
 #[derive(PartialEq, Debug)]
-enum Value<S>
-where
-    S: Eq + Hash,
-{
+enum Value {
     Number(f64),
-    String(S),
+    String(String),
     Bool(bool),
     Null,
-    Object(HashMap<S, Value<S>>),
-    Array(Vec<Value<S>>),
+    Object(HashMap<String, Value>),
+    Array(Vec<Value>),
 }
 
-fn lex<'a, P>(p: P) -> impl Parser<Input = P::Input, Output = P::Output>
+fn lex<Input, P>(p: P) -> impl Parser<Input, Output = P::Output>
 where
-    P: Parser,
-    P::Input: RangeStream<Item = u8, Range = &'a [u8]>,
-    <P::Input as StreamOnce>::Error: ParseError<
-        <P::Input as StreamOnce>::Item,
-        <P::Input as StreamOnce>::Range,
-        <P::Input as StreamOnce>::Position,
+    P: Parser<Input>,
+    Input: Stream<Token = char>,
+    <Input as StreamOnce>::Error: ParseError<
+        <Input as StreamOnce>::Token,
+        <Input as StreamOnce>::Range,
+        <Input as StreamOnce>::Position,
     >,
 {
-    no_partial(p.skip(range::take_while(|b| {
-        b == b' ' || b == b'\t' || b == b'\r' || b == b'\n'
-    })))
+    p.skip(spaces())
 }
 
-fn digits<'a, I>() -> impl Parser<Input = I, Output = &'a [u8]> + 'a
+fn integer<Input>() -> impl Parser<Input, Output = i64>
 where
-    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    range::take_while1(|b| b >= b'0' && b <= b'9')
+    lex(many1(digit()))
+        .map(|s: String| {
+            let mut n = 0;
+            for c in s.chars() {
+                n = n * 10 + (c as i64 - '0' as i64);
+            }
+            n
+        })
+        .expected("integer")
 }
 
-fn number<'a, I>() -> impl Parser<Input = I, Output = f64> + 'a
+fn number<Input>() -> impl Parser<Input, Output = f64>
 where
-    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    no_partial(
-        lex(range::recognize(no_partial((
-            optional(one_of("+-".bytes())),
-            byte(b'0').or((digits(), optional((byte(b'.'), digits()))).map(|_| b'0')),
-            optional((
-                (one_of("eE".bytes()), optional(one_of("+-".bytes()))),
-                digits(),
-            )),
-        )))).map(|s: &'a [u8]| str::from_utf8(s).unwrap().parse().unwrap())
-            .expected("number"),
-    )
-}
-
-fn json_string<'a, I>() -> impl Parser<Input = I, Output = &'a str>
-where
-    I: RangeStream<Item = u8, Range = &'a [u8]>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    let back_slash_byte = satisfy_map(|c| {
-        Some(match c {
-            b'"' => b'"',
-            b'\\' => b'\\',
-            b'/' => b'/',
-            b'b' => '\u{0008}' as u8,
-            b'f' => '\u{000c}' as u8,
-            b'n' => b'\n',
-            b'r' => b'\r',
-            b't' => b'\t',
-            _ => return None,
+    let i = char('0').map(|_| 0.0).or(integer().map(|x| x as f64));
+    let fractional = many(digit()).map(|digits: String| {
+        let mut magnitude = 1.0;
+        digits.chars().fold(0.0, |acc, d| {
+            magnitude /= 10.0;
+            match d.to_digit(10) {
+                Some(d) => acc + (d as f64) * magnitude,
+                None => panic!("Not a digit"),
+            }
         })
     });
-    let inner = range::recognize(escaped(
-        range::take_while1(|b| b != b'\\' && b != b'"'),
-        b'\\',
-        back_slash_byte,
-    )).map(|s| str::from_utf8(s).unwrap());
-    between(byte(b'"'), lex(byte(b'"')), inner).expected("string")
+
+    let exp = satisfy(|c| c == 'e' || c == 'E').with(optional(char('-')).and(integer()));
+    lex(optional(char('-'))
+        .and(i)
+        .map(|(sign, n)| if sign.is_some() { -n } else { n })
+        .and(optional(char('.')).with(fractional))
+        .map(|(x, y)| if x >= 0.0 { x + y } else { x - y })
+        .and(optional(exp))
+        .map(|(n, exp_option)| match exp_option {
+            Some((sign, e)) => {
+                let e = if sign.is_some() { -e } else { e };
+                n * 10.0f64.powi(e as i32)
+            }
+            None => n,
+        }))
+    .expected("number")
 }
 
-fn object<'a, I>() -> impl Parser<Input = I, Output = HashMap<&'a str, Value<&'a str>>>
+fn json_char<Input>() -> impl Parser<Input, Output = char>
 where
-    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    let field = (json_string(), lex(byte(b':')), json_value_()).map(|t| (t.0, t.2));
-    let fields = sep_by(field, lex(byte(b',')));
-    between(lex(byte(b'{')), lex(byte(b'}')), fields).expected("object")
+    parser(|input: &mut Input| {
+        let (c, committed) = any().parse_lazy(input).into_result()?;
+        let mut back_slash_char = satisfy_map(|c| {
+            Some(match c {
+                '"' => '"',
+                '\\' => '\\',
+                '/' => '/',
+                'b' => '\u{0008}',
+                'f' => '\u{000c}',
+                'n' => '\n',
+                'r' => '\r',
+                't' => '\t',
+                _ => return None,
+            })
+        });
+        match c {
+            '\\' => committed.combine(|_| back_slash_char.parse_stream(input).into_result()),
+            '"' => Err(Commit::Peek(Input::Error::empty(input.position()).into())),
+            _ => Ok((c, committed)),
+        }
+    })
 }
 
-fn array<'a, I>() -> impl Parser<Input = I, Output = Vec<Value<&'a str>>>
+fn json_string<Input>() -> impl Parser<Input, Output = String>
 where
-    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    between(
-        lex(byte(b'[')),
-        lex(byte(b']')),
-        sep_by(json_value_(), lex(byte(b','))),
-    ).expected("array")
+    between(char('"'), lex(char('"')), many(json_char())).expected("string")
 }
 
-#[inline(always)]
-fn json_value<'a, I>() -> impl Parser<Input = I, Output = Value<&'a str>>
+fn object<Input>() -> impl Parser<Input, Output = Value>
 where
-    I: RangeStream<Item = u8, Range = &'a [u8]>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    spaces().with(json_value_())
+    let field = (json_string(), lex(char(':')), json_value()).map(|t| (t.0, t.2));
+    let fields = sep_by(field, lex(char(',')));
+    between(lex(char('{')), lex(char('}')), fields)
+        .map(Value::Object)
+        .expected("object")
+}
+
+#[inline]
+fn json_value<Input>() -> impl Parser<Input, Output = Value>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    json_value_()
 }
 
 // We need to use `parser!` to break the recursive use of `value` to prevent the returned parser
 // from containing itself
-parser!{
-    #[inline(always)]
-    fn json_value_['a, I]()(I) -> Value<&'a str>
-        where [ I: RangeStream<Item = u8, Range = &'a [u8]> + 'a ]
+parser! {
+    #[inline]
+    fn json_value_[Input]()(Input) -> Value
+        where [ Input: Stream<Token = char> ]
     {
+        let array = between(
+            lex(char('[')),
+            lex(char(']')),
+            sep_by(json_value(), lex(char(','))),
+        ).map(Value::Array);
+
         choice((
             json_string().map(Value::String),
-            object().map(Value::Object),
-            array().map(Value::Array),
+            object(),
+            array,
             number().map(Value::Number),
-            lex(range::range(&b"false"[..]).map(|_| Value::Bool(false))),
-            lex(range::range(&b"true"[..]).map(|_| Value::Bool(true))),
-            lex(range::range(&b"null"[..]).map(|_| Value::Null)),
+            lex(string("false").map(|_| Value::Bool(false))),
+            lex(string("true").map(|_| Value::Bool(true))),
+            lex(string("null").map(|_| Value::Null)),
         ))
     }
 }
@@ -185,9 +216,10 @@ fn json_test() {
             ("true", Bool(true)),
             ("false", Bool(false)),
             ("null", Null),
-        ].into_iter()
-            .map(|(k, v)| (k.to_string(), v))
-            .collect(),
+        ]
+        .into_iter()
+        .map(|(k, v)| (k.to_string(), v))
+        .collect(),
     );
     match result {
         Ok(result) => assert_eq!(result, (expected, "")),
@@ -201,9 +233,7 @@ fn json_test() {
 fn parse(b: &mut Bencher, buffer: &str) {
     let mut parser = json_value();
     b.iter(|| {
-        let buf = black_box(buffer.as_bytes());
-
-        let result = parser.easy_parse(buf).unwrap();
+        let result = parser.easy_parse(position::Stream::new(&buffer[..]));
         black_box(result)
     });
 }
